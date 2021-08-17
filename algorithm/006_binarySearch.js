9814b2a920c793a8590fb575481a9c15f5abfb4b
59inu <59inu.dev@gmail.com>
2021-08-18 06:43:27 +0900

    comment: [006] 바이너리서치 주석 추가

--- algorithm/006_binarySearch.js
+++ algorithm/006_binarySearch.js
@@ -2,6 +2,13 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
+ *
+ * key: 매 단계마다 탐색 범위를 절반으로 줄여나간다.
+ *
+ * 1. 배열 요소가 정렬 상태일 때
+ * 2. 특정 값의 위치를 찾을 때
+ *
+ * big-O: O(log n)
  */
 var search = function (nums, target) {
   let left = 0;
